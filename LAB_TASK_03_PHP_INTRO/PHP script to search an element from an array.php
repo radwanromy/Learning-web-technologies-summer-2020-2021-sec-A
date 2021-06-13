cf373af0945ebff175b9edb9232db7b717d7f456
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2021-06-13 14:51:30 +0600

    Last

--- LAB_TASK_03_PHP_INTRO/PHP script to search an element from an array.php
+++ LAB_TASK_03_PHP_INTRO/PHP script to search an element from an array.php
@@ -3,13 +3,28 @@
     <meta charset="UTF-8">
     <title>Document</title>
 </head>
+<body>
+    <html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
 <body><?php
 	
 $Array = ["Eggs", "Bacon", "HashBrowns", "Beans", "Bread", "RedSauce"];
-
+  $search="Beans";
         for ($i = 0; $i < count($Array); $i++)  {
-            echo $Array[$i] ."<br>";
-        }
+        if($Array[$i]==$search)
+            {
+            echo "Found : ".$Array[$i] ."<br>";
+            }
+            else
+            {
+               //echo "Not Found";
+            }
+        }  
 ?>
+</body>
+</html>
 </body>
 </html>
