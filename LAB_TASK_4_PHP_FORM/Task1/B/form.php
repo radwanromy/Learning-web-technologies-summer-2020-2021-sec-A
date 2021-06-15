2ceb3adcd246c13e7f3e7f23f59842c30f44f3ec
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2021-06-15 18:43:33 +0600

    Task1B

---
+++ LAB_TASK_4_PHP_FORM/Task1/B/form.php
@@ -0,0 +1,30 @@
+<?php
+	
+	if(isset($_REQUEST['submit'])){
+		
+		$email = $_REQUEST['email'];
+
+		if($username == ""){
+			echo "invalid email!";
+		}else{
+			echo $email;
+		}
+
+	}else{
+		echo 'invalid request';
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Data</title>
+</head>
+<body>
+	<form method="REQUEST" action="email.php">
+
+		<fieldset><legend>Email</legend> <input type="email" name="username" value="" /><br>
+				<input type="submit" name="submit" value="Submit"></fieldset>
+	</form>
+</body>
+</html>
