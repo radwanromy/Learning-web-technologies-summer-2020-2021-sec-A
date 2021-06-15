3e28d320b1a65a357b36ef9c3a38fe6e793dc4cf
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2021-06-15 19:27:00 +0600

    LAB_TASK_4_PHP_FORM Task 2 B

---
+++ LAB_TASK_4_PHP_FORM/Task2/B/form.php
@@ -0,0 +1,30 @@
+<?php
+	
+	if(isset($_REQUEST['submit'])){
+		
+		$dob = $_REQUEST['dob'];
+
+		if($dob == ""){
+			echo "invalid email!";
+		}else{
+			echo $dob;
+		}
+
+	}else{
+		echo 'invalid request';
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Data</title>
+</head>
+<body>
+		<form method="post">
+
+		<fieldset><legend>Date Of Birth</legend> <input type="date" name="dob" value="" /><br>
+				<input type="submit" name="submit" value="Submit"></fieldset>
+	</form>
+</body>
+</html>
