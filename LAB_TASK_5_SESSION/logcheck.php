754c2dc0ff6c3fb7c8feccc5ba75165d482a91c3
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2021-06-20 22:33:08 +0600

    LAB_TASK_5 Regi php

---
+++ LAB_TASK_5_SESSION/logcheck.php
@@ -0,0 +1,27 @@
+<?php
+	session_start();
+
+	if(isset($_GET['submit']))
+    {
+
+		$uname= $_GET['uname'];
+		$password= $_GET['password'];
+        //$remember= $_POST['check'];
+
+		if($uname != '' && $password != '')
+        {
+			if($_SESSION['user']['uname'] == $uname && $_SESSION['user']['password'] == $password){
+					$_SESSION['flag'] = 'true';
+					header('location: Dashboard.html');
+			}else{
+				echo 'invlaid username/password';
+			}
+
+		}else{
+			echo "null value found...";
+		}
+	}else{
+		echo "invalid request...";
+	}
+
+?>
---
+++ LAB_TASK_5_SESSION/registration.php
@@ -0,0 +1,27 @@
+<?php
+	session_start();
+
+	if(isset($_POST['submit'])){
+
+        $name       = $_POST['name'];
+		$uname 		= $_POST['uname'];
+		$email 		= $_POST['email'];
+		$password 	= $_POST['password'];
+		$gender     = $_POST['gender'];
+		$day        = $_POST['day'];
+		$month      = $_POST['month'];
+		$year       = $_POST['year'];
+        
+		if($name != '' && $uname != '' && $password != '' && $email != '' && $gender !='' && $day != '' && $month != '' && $year != ''  )
+        {
+            $user =['name'=>$name, 'email'=>$email , 'uname'=> $uname , 'password'=>$password ,  'gender'=>$gender , 'day'=>$day , 'month'=>$month, 'year'=>$year ];
+			$_SESSION['user'] = $user;
+			header('location: Login.html');
+		}else{
+			echo "null value found...";
+		}
+	}else{
+		header('location: Login.html');
+	}
+
+?>
