ffad8a6e6d765c9b8516a204251b319af75354dd
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2021-07-29 20:24:58 +0600

    Final_Lab_Task_Ajax_04

---
+++ Final_Lab_Task_Ajax/userController.php
@@ -0,0 +1,63 @@
+<?php 
+	session_start();
+	require_once('session_header.php');
+	require_once('subDb.php');
+
+
+	//add user
+	if(isset($_POST['create'])){
+		$username 	= $_POST['username'];
+		$password 	= $_POST['password'];
+		$email 		= $_POST['email'];
+
+		if(empty($username) || empty($password) || empty($email)){
+			header('register.php?error=null_value');
+		}else{
+
+			$user = [
+				'username'=> $username,
+				'password'=> $password,
+				'email'=> $email
+			];
+
+			$status = insert($user);
+
+			if($status){
+				header('all_users.php?success=done');
+			}else{
+				header('create.php?error=db_error');
+			}
+		}
+	}
+
+	//update user
+	if(isset($_POST['edit'])){
+
+		$username 	= $_POST['username'];
+		$password 	= $_POST['password'];
+		$email 		= $_POST['email'];
+		$id 		= $_POST['id'];
+
+		if(empty($username) || empty($password) || empty($email)){
+			header('edit.php?id={$id}');
+		}else{
+
+			$user = [
+				'username'=> $username,
+				'password'=> $password,
+				'email'=> $email,
+				'id'=> $id
+			];
+
+			$status = update($user);
+
+			if($status){
+				header('all_users.php?success=done');
+			}else{
+				header('edit.php?id={$id}');
+				header('edit.php?id={$id}');
+			}
+		}
+	}
+
+?>
