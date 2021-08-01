c64a29479d5372add39f233d9a0823bc821e656a
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2021-08-01 09:53:23 +0600

    Mini Project 02

---
+++ MINI_PROJECT/admin_Home.php
@@ -0,0 +1,31 @@
+<?php
+
+?>  	
+<!DOCTYPE html>
+<html>
+<head>
+	<title>User Profile</title>
+</head>
+<body>
+	<fieldset> 
+
+		<table width="50%" height="50%">
+			<tr>
+				<td align="center">
+					
+					<a href="Profile.html">Profile</a>   <br>
+					<a href="changepass.html">Change password</a> <br>
+					<a href="users.html">View Users</a><br> 
+					<a href="logout.php">Log out</a>        
+				</td>
+			</tr>
+		</table>
+		</fieldset>
+</body>
+</html>
+
+
+<?php
+
+
+?>
